c298ec2274f6fe7f33351efb5247f67884ee7ef2
Dan Sehl <dan.sehl@gmail.com>
2020-09-19 13:42:03 -0600

    Add map service

---
+++ app/services/map_service.rb
@@ -0,0 +1,14 @@
+class MapService
+  def self.get_coords(location)
+    response = conn.get do |f|
+      f.params['location'] = location
+    end
+    JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
+  end
+
+  def self.conn
+    Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/address') do |f|
+      f.params['key'] = ENV['MAP_KEY']
+    end
+  end
+end
