1e8ca8df1c45849ebb98511d5f41422a4663983d
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 10:23:37 -0600

    Refactor climbing routes facade

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -2,7 +2,7 @@ class ClimbingRoutesFacade
   def self.generate_route(location)
     coords = MapService.get_coords(location)
     weather_data = WeatherService.get_weather(coords)
-    routes = RoutesService.get_routes(coords)
+    routes = ClimbingRoutesService.get_routes(coords)
     ClimbingRoute.new(weather_data, location, routes)
   end
 end
