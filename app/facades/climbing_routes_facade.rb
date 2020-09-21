cf2ecd1988b729172241921f5e43ce057788c831
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 10:00:56 -0600

    Refactpr climbing routes facade

--- app/facades/climbing_routes_facade.rb
+++ app/facades/climbing_routes_facade.rb
@@ -2,7 +2,7 @@ class ClimbingRoutesFacade
   def self.generate_route(location)
     coords = MapService.get_coords(location)
     weather_data = WeatherService.get_weather(coords)
-    # some other code to get routes from a service
-    ClimbingRoute.new(weather_data, location)
+    routes = RoutesService.get_routes(coords)
+    ClimbingRoute.new(weather_data, location, routes)
   end
 end
