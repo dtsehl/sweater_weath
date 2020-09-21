19ca0f64e82761136372f20e9a647a5c573182d3
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 09:29:55 -0600

    Add climbing routes show route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get 'backgrounds', to: 'backgrounds#show'
       post 'users', to: 'users#create'
       post 'sessions', to: 'sessions#create'
+      get 'climbing_routes', to: 'climbing_routes#show'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
