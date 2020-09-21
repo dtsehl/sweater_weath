9fe041c586255f98d64dad75f8b78ef74f551ad6
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 09:28:40 -0600

    Create climbing routes request spec with pry at the end

---
+++ spec/requests/api/v1/routes_request_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Api::V1::Routes', type: :request do
+  describe 'Climbing Routes request API', :vcr do
+    it 'can return correct route data' do
+      get '/api/v1/climbing_routes?location=erwin,tn'
+
+      
+
+      json = JSON.parse(response.body, symbolize_names: true)
+      expect(response).to be_successful
+      require "pry"; binding.pry
+    end
+  end
+end
