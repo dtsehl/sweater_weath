6c14525dac99ef5c0efc11b2868b617f178811b5
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 09:59:42 -0600

    Refactor climbing routes service spec

--- spec/services/routes_service_spec.rb
+++ spec/services/routes_service_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Routes Service' do
-  it 'can get routes nearby from lat/long coordinates' do
+  it 'can get routes nearby from lat/long coordinates', :vcr do
     coords = { lat: 39.738453, lng: -104.984853 }
     routes = ClimbingRoutesService.get_routes(coords)
 
     expect(routes).to be_a(Hash)
-    require "pry"; binding.pry
-    # 
+    expect(routes[:routes]).to be_a(Array)
+    expect(routes[:routes][0]).to have_key(:id)
+    expect(routes[:routes][0]).to have_key(:name)
+    expect(routes[:routes][0]).to have_key(:type)
+    expect(routes[:routes][0]).to have_key(:location)
+    expect(routes[:routes][0][:name]).to be_a(String)
+    expect(routes[:routes][0][:id]).to be_a(Integer)
+    expect(routes[:routes][0][:location]).to be_a(Array)
   end
 end
