cb45fd198f6f85ec629c5e8e7bcbede7b2f6ca5c
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 09:29:39 -0600

    Remove VCR from routes request spec

--- spec/requests/api/v1/routes_request_spec.rb
+++ spec/requests/api/v1/routes_request_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'Api::V1::Routes', type: :request do
-  describe 'Climbing Routes request API', :vcr do
+  describe 'Climbing Routes request API' do
     it 'can return correct route data' do
       get '/api/v1/climbing_routes?location=erwin,tn'
 
-      
+
 
       json = JSON.parse(response.body, symbolize_names: true)
       expect(response).to be_successful
