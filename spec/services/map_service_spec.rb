82bcfdf7bfd942edaaffccfd597442aa2b5e8993
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 10:56:33 -0600

    Add get distance between test in map service spec

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe 'Map Service', :vcr do
     expect(location[:lat]).to eq(41.883229)
     expect(location[:lng]).to eq(-87.632398)
   end
+  it 'can get the distance between two lat/long coordinates' do
+    starting_coords = { lat: 39.738453, lng: -104.984853 }
+    ending_coords = { lat: 39.9312, lng: -105.2852 }
+    distance_between = MapService.get_distance_between(starting_coords, ending_coords)
+
+    expect(distance_between).to be_a(Float)
+  end
 end
