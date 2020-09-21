340e1341fd157b401de67876a8956d75677b6c95
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 10:23:12 -0600

    Refactor climbing route poro spec

--- spec/poros/climbing_route_spec.rb
+++ spec/poros/climbing_route_spec.rb
@@ -2,19 +2,27 @@ require 'rails_helper'
 
 describe ClimbingRoute do
   it 'initializes with attributes' do
-    data = JSON.parse(File.read('spec/fixtures/json/routes_output.json'))
-    require "pry"; binding.pry
+    coords = { lat: 39.738453, lng: -104.984853 }
+    data = ClimbingRoutesService.get_routes(coords)
     location = 'denver,co'
-    weather = JSON.parse(File.read('spec/fixtures/json/routes_output.json', symbolize_names: true))
-    route = ClimbingRoute.new(data, location, weather)
-    expect(forecast.id).to be_nil
-    expect(forecast.timezone).to eq(data[:timezone])
-    expect(forecast.current[:temp]).to eq(data[:current][:temp])
-    expect(forecast.daily[0][:temp][:min]).to eq(data[:daily][0][:temp][:min])
-    expect(forecast.hourly[0][:dt]).to eq(data[:hourly][0][:dt])
+    weather = WeatherService.get_weather(coords)
+    climbing_route = ClimbingRoute.new(data, location, weather)
+
+    expect(climbing_route.id).to be_nil
+    expect(climbing_route.location).to be_a(String)
+    expect(climbing_route.forecast).to be_a(Hash)
+    expect(climbing_route.forecast[:summary]).to be_a(String)
+    expect(climbing_route.forecast[:temperature]).to be_a(String)
+    expect(climbing_route.routes).to be_a(Array)
+    expect(climbing_route.routes[0][:name]).to be_a(String)
+    expect(climbing_route.routes[0][:type]).to be_a(String)
+    expect(climbing_route.routes[0][:rating]).to be_a(String)
+    expect(climbing_route.routes[0][:distance_to_route]).to be_a(String)
+    expect(climbing_route.routes[0][:location]).to be_a(Array)
+    expect(climbing_route.routes[0][:location][0]).to be_a(String)
   end
-  it 'only has the attributes required' do
-    data = JSON.parse(File.read('spec/fixtures/json/routes_output.json')
+  xit 'only has the attributes required' do
+    data = JSON.parse(File.read('spec/fixtures/json/routes_output.json'))
     location = 'denver,co'
     forecast = Forecast.new(data, location)
     expect(forecast.current[:clouds]).to_not eq(data[:current][:clouds])
