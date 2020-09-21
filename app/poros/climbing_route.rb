b374b6e00ba3b930722e46ed0e9714fa48448f51
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 11:33:32 -0600

    Refactored the prune_routes method in Climbing Routes poro

--- app/poros/climbing_route.rb
+++ app/poros/climbing_route.rb
@@ -17,13 +17,15 @@ class ClimbingRoute
   end
 
   def prune_weather(weather)
-    {summary: weather[:current][:weather][0][:description], temperature: weather[:current][:temp].to_i}
+    { summary: weather[:current][:weather][0][:description], temperature: weather[:current][:temp].to_i }
   end
 
   def prune_routes(routes, starting_coords)
-    require "pry"; binding.pry
-    ending_coords = {lat: routes[:routes][0][:latitude], lng: routes[:routes][0][:longitude]}
-    
-    data.without(:pressure, :dew_point, :clouds, :wind_speed, :wind_deg)
+    all_routes = []
+    routes[:routes].each do |route|
+      ending_coords = { lat: route[:latitude], lng: route[:longitude] }
+      all_routes << { name: route[:name], type: route[:type], rating: route[:stars], location: route[:location], distance_to_route: MapService.get_distance_between(starting_coords, ending_coords) }
+    end
+    all_routes
   end
 end
