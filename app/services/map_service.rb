53a02f352398ae4b59c2ea219f9fc3bcd1c29c29
Dan Sehl <dan.sehl@gmail.com>
2020-09-21 10:57:48 -0600

    Add get distance_between method in map service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -1,13 +1,23 @@
 class MapService
   def self.get_coords(location)
-    response = conn.get do |f|
+    response = conn.get 'geocoding/v1/address' do |f|
       f.params['location'] = location
     end
     JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
   end
 
+  def self.get_distance_between(starting_coords, ending_coords)
+    starting_coords = starting_coords[:lat].to_s + ',' + starting_coords[:lng].to_s
+    ending_coords = ending_coords[:lat].to_s + ',' + ending_coords[:lng].to_s
+    response = conn.get 'directions/v2/route' do |f|
+      f.params['from'] = starting_coords
+      f.params['to'] = ending_coords
+    end
+    JSON.parse(response.body, symbolize_names: true)[:route][:distance]
+  end
+
   def self.conn
-    Faraday.new(url: 'http://www.mapquestapi.com/geocoding/v1/address') do |f|
+    Faraday.new(url: 'http://www.mapquestapi.com') do |f|
       f.params['key'] = ENV['MAP_KEY']
     end
   end
