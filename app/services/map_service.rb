f73599a68d2643e0886609d0456cbded7717149e
Dan Sehl <dan.sehl@gmail.com>
2020-09-22 14:44:07 -0600

    Add get_route_between method to map service

--- app/services/map_service.rb
+++ app/services/map_service.rb
@@ -6,6 +6,14 @@ class MapService
     JSON.parse(response.body, symbolize_names: true)[:results][0][:locations][0][:latLng]
   end
 
+  def self.get_route_between(origin, destination)
+    response = conn.get 'directions/v2/route' do |f|
+      f.params['from'] = origin
+      f.params['to'] = destination
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
   def self.conn
     Faraday.new(url: 'http://www.mapquestapi.com') do |f|
       f.params['key'] = ENV['MAP_KEY']
