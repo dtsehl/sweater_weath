b7f68083eb2eea7529e9252d9aa9348bc163124b
Dan Sehl <dan.sehl@gmail.com>
2020-09-22 16:47:40 -0600

    Fix indentation in road trip facade

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,7 +1,7 @@
 class RoadTripFacade
   def self.generate_road_trip(origin, destination)
-      route_data = MapService.get_route_between(origin, destination)
-      destination_weather_data = WeatherService.get_destination_forecast(route_data)
-      RoadTrip.new(route_data, destination_weather_data, origin, destination)
+    route_data = MapService.get_route_between(origin, destination)
+    destination_weather_data = WeatherService.get_destination_forecast(route_data)
+    RoadTrip.new(route_data, destination_weather_data, origin, destination)
   end
 end
