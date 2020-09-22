bf67ad7ec558949c1d14dbc12180327e3e5d54ec
Dan Sehl <dan.sehl@gmail.com>
2020-09-22 14:44:23 -0600

    Add road trip create route in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
       get 'backgrounds', to: 'backgrounds#show'
       post 'users', to: 'users#create'
       post 'sessions', to: 'sessions#create'
+      post 'road_trip', to: 'road_trip#create'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
