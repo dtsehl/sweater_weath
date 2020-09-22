f34a2ebbb5b98084c6310b3bdceb5f6124f03b77
Dan Sehl <dan.sehl@gmail.com>
2020-09-22 16:41:59 -0600

    Refactor road trip facade to instantiate a road trip with more data

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -2,6 +2,6 @@ class RoadTripFacade
   def self.generate_road_trip(origin, destination)
       route_data = MapService.get_route_between(origin, destination)
       destination_weather_data = WeatherService.get_destination_forecast(route_data)
-      RoadTrip.new(route_data, destination_weather_data)
+      RoadTrip.new(route_data, destination_weather_data, origin, destination)
   end
 end
